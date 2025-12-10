86cc71a7d0587f5e3ca6940de90abad06e418bef
timarmstead <tim@crm360.agency>
2025-12-10 09:57:51 +0000

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !! Dangerously allow production builds to complete even if
+    // your project has type errors. !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

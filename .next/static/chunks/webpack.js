27e37641bcf5d06dc82d7f4081e7c25aee6b775e
Tim Armstead <timothyarmstead@TIMOTHYs-Air.lan>
2026-02-16 17:05:50 +0000

    Add robots.txt to fix sitemap discovery

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f77ffc8dc5e6153"; }
+/******/ 		__webpack_require__.h = function() { return "13ede8d4741eb28f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

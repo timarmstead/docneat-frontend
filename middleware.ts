cd27f9c53ca2bc573ac89574c4be1ce4122b3dd5
Tim Armstead <timothyarmstead@TIMOTHYs-Air.lan>
2026-02-21 21:50:18 +0000

    fix: align middleware and layout keys

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,14 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware((auth, req) => {
 }, {
-  // We force it to use the new name we just created
-  secretKey: process.env.LIVE_CLERK_SECRET_KEY,
-  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  // Hard-code the keys here as well to match your Layout exactly
+  publishableKey: "pk_live_Y2xlcmsuZG9jbmVhdC5jb20k",
+  secretKey: process.env.CLERK_SECRET_KEY, 
 });
 
 export const config = {
-  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
